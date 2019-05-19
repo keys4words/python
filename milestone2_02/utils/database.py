9fb37127645011313268f9035298af0df142238d
Max 19038 <keys4words@gmail.com>
2019-05-19 17:28:22 +0300

    add typing in functions

--- milestone2_02/utils/database.py
+++ milestone2_02/utils/database.py
@@ -1,18 +1,21 @@
+from typing import List, Dict, Union
 from .db_connection import DatabaseConnection
 
-def create_book_table():
+Book = Dict[str, Union[str, int]]
+
+def create_book_table() -> None:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
         cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key, author text, read integer)')
 
 
-def add_book(name, author):
+def add_book(name: str, author: str) -> None:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
         cursor.execute('INSERT INTO books VALUES(?, ?, 0)', (name, author))
 
 
-def get_all_books():
+def get_all_books() -> List[Book]:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
         cursor.execute('SELECT * FROM books')
@@ -20,13 +23,13 @@ def get_all_books():
     return books
 
 
-def mark_book_as_read(name):
+def mark_book_as_read(name: str) -> None:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
         cursor.execute('UPDATE books SET read=1 WHERE name=?', (name,))
 
 
-def delete_book(name):
+def delete_book(name: str) -> None:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
         cursor.execute('DELETE FROM books WHERE name=?', (name,))
