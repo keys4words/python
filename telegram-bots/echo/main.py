b5cba87193ff0263d45134c35cfd03f42c2653e3
keys4words <keys4words@gmail.com>
2020-04-30 17:13:53 +0300

    add log func

--- telegram-bots/echo/main.py
+++ telegram-bots/echo/main.py
@@ -1,20 +1,30 @@
 from telegram import Bot, Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from echo.config import MY_TOKEN, BASE_URL
+from echo.config import MY_TOKEN, BASE_URL, PROXY
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log')
 
 def do_start(bot: Bot, update: Update):
     bot.send_message(
         chat_id=update.message.chat_id,
         text='Hey! Send me something',
     )
+    logging.info('Bot started!')
 
 def do_echo(bot: Bot, update: Update):
-    chat_id = update.message.chat_id
-    text = "Your ID={}\nYour message - {}".format(chat_id, update.message.text)
+    u_m = update.message.chat
+    chat_id = u_m.id
+    full_name = u_m.first_name + ' ' + u_m.last_name
+    text = "{} написал\n'{}'".format(full_name, update.message.text)
     bot.send_message(
         chat_id=chat_id,
         text=text,
     )
+    logging.info("User: %s, Chat id: %s, Message: %s",
+                 full_name, chat_id, update.message.text)
 
 
 def main():
@@ -25,10 +35,15 @@ def main():
     updater = Updater(
         bot=bot,
     )
+    # updater = Updater(
+    #     bot=bot,
+    #     request_kwargs=PROXY
+    # )
     start_handler = CommandHandler("start", do_start)
     message_handler = MessageHandler(Filters.text, do_echo)
-    updater.dispatcher.add_handler(start_handler)
-    updater.dispatcher.add_handler(message_handler)
+    u_d = updater.dispatcher
+    u_d.add_handler(start_handler)
+    u_d.add_handler(message_handler)
 
     updater.start_polling()
     updater.idle()
