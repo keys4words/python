08431836498e6caed8e01cbc3548b295b69056fe
keys4words <keys4words@gmail.com>
2020-04-30 17:40:24 +0300

    Move telegram bots to other folder

--- telegram-bots/echo/config.py
+++
@@ -1,4 +0,0 @@
-MY_TOKEN = "1123464143:AAFXkUVmLbETZuGTtHuUylvIuFzgFl63kr8"
-BASE_URL = "https://telegg.ru/orig/bot"
-PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
-         'urllib3_proxy_kwargs': {'username': 'learn', 'passwords': 'python'}}
